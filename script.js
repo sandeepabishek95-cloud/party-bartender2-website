48e81d0c728c2a1f9180674aca8ca016d1b2c61a
sandeepabishek95-cloud <sandeepabishek95@gmail.com>
2026-03-28 11:26:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-// Cocktail dynamic page
-const params = new URLSearchParams(window.location.search);
-const name = params.get("name");
+const params=new URLSearchParams(window.location.search);
+const name=params.get("name");
 
 if(name){
-  const title = document.getElementById("title");
-  const video = document.getElementById("video");
-  const list = document.getElementById("ingredients");
+document.getElementById("title").innerText=name;
 
-  if(title) title.innerText = name;
+document.getElementById("video").src=
+"https://www.w3schools.com/html/mov_bbb.mp4";
 
-  if(video){
-    video.src = "https://www.w3schools.com/html/mov_bbb.mp4";
-  }
+const ingredients=[
+"Vodka","Lime Juice","Mint Leaves","Sugar Syrup",
+"Ice Cubes","Soda Water","Salt","Orange Garnish"
+];
 
-  const items = [
-    "Vodka","Lime","Mint","Sugar",
-    "Ice","Soda","Salt","Fruit Garnish"
-  ];
+const list=document.getElementById("ingredients");
 
-  if(list){
-    items.forEach(i=>{
-      let li = document.createElement("li");
-      li.innerText = i;
-      list.appendChild(li);
-    });
-  }
+ingredients.forEach(i=>{
+let li=document.createElement("li");
+li.innerText=i;
+list.appendChild(li);
+});
 }
